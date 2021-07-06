5f9c20a7446c3bc82acb8d91cd760d1f7237d64f
Beth <bethm.meeker@gmail.com>
2021-07-06 16:54:56 -0600

    Remove unused and add folders

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>Putrid Physalis Philadelphica</h1>
+    );
+  }
+
+}
+
+export default App;
