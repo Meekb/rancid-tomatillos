be3618a2a698b7308ffbd3ac3a39443f8180aaed
Em Orendorff <em.orendorff@gmail.com>
2021-07-08 09:17:59 -0500

    Add hover state and flex to Movie component

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,12 +3,13 @@ import './Movie.css'
 
 const Movie = ( {title, posterPath} ) => {
     return (
-        <section>
-            <img src={posterPath}/>
+        <section className='each-movie'>
+            <img src={posterPath} className='zoom'/>
             <p> {title} </p>
             {/* <p>I am movie hear me roar</p> */}
         </section>
     )
+
 }
 
 export default Movie
