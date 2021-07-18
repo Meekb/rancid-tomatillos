453c960e96e7326e1ca261bf6059699183c219ee
Beth <bethm.meeker@gmail.com>
2021-07-18 13:44:54 -0600

    Create fixture and command

--- cypress/integration/moviePoster_spec.js
+++ cypress/integration/moviePoster_spec.js
@@ -1,36 +1,19 @@
-describe('Single movie poster view', () => {
+// import posterDetails from './cypress/fixtures/posterDetails.json'
+
+describe('Poster view flow', () => {
 
   beforeEach(() => {
-    cy.fixture('posterDetails')
-    .then(movieDetails => {
-      const url = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
-      cy.intercept('GET', `${url}/337401`), {
-        statusCode: 201,
-        body: posterDetails
-      }
-    })
     cy.visit('http://localhost:3000/')
-  }
-
-  it('Should render the movie cover', () => {
-    
+    cy.get('img').click()
+    // cy.click('img')
+    cy.stubMoviePoster()
   });
 
-  // it('Should render an area that displays the movie details', () => {
-    
-  // });
-
-  // it('Should render details unavailable for any category where details are unavailable', () => {
-
-  // });
-
-  // it('Should display the movie cover next to the movie details', () => {
-
-  // });
-
-  // it('Should render the movie relase date into a MM-DD-YYYY format', () => {
-
-  // });
+  it('Should render the movie poster for Mulan', () => {
+    cy.get('poster-title').contains('Mulan')
+  })
 
 
-});
+})
+
+
